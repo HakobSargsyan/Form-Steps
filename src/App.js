50a70e0a8d5256d93f5c4c0a622dd25839fd2141
Hakob <hakobsargsyan1993@gmail.com>
2022-06-06 11:39:08 +0400

    forgotten file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import FormFlow from "./components/FormFlowContainer";
+import PageNotFound from "./components/PageNotFound";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Navigate
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={ <Navigate to="/flow/1" /> } />
+                <Route path="/flow" element={<FormFlow />}>
+                    <Route path="/flow:step" element={<FormFlow />}></Route>
+                </Route>
+                <Route path="/404" element={<PageNotFound />}></Route>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
